d974d0532ee20f74aba80f10a9577f118f03d463
HengJay <hengjay.wang@itri.org.tw>
2022-01-11 15:10:59 +0800

    iOS 15 Update - Async/Await of NetworkManager.

--- Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-final class AppetizerListViewModel: ObservableObject {
+@MainActor final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
     @Published var alertItem: AlertItem?
@@ -15,16 +15,45 @@ final class AppetizerListViewModel: ObservableObject {
     @Published var isShowingDetail = false
     @Published var selectedAppetizer: Appetizer?
     
+    
+    /// Old version getAppetizers() without using async/await
+//    func getAppetizers() {
+//        isLoading = true
+//        NetworkManager.shared.getAppetizers { [self] result in
+//            DispatchQueue.main.async {
+//                isLoading = false
+//                switch result {
+//                case .success(let appetizers):
+//                    self.appetizers = appetizers
+//                case .failure(let error):
+//                    switch error {
+//                    case .invalidURL:
+//                        alertItem = AlertContext.invalidURL
+//
+//                    case .invalidResponse:
+//                        alertItem = AlertContext.invalidResponse
+//
+//                    case .invalidData:
+//                        alertItem = AlertContext.invalidData
+//
+//                    case .unableToComplete:
+//                        alertItem = AlertContext.unableToComplete
+//                    }
+//                }
+//            }
+//        }
+//    }
+    
+    
+    /// New verision getAppetizers() with Async/Await
     func getAppetizers() {
         isLoading = true
-        NetworkManager.shared.getAppetizers { [self] result in
-            DispatchQueue.main.async {
-                isLoading = false
-                switch result {
-                case .success(let appetizers):
-                    self.appetizers = appetizers
-                case .failure(let error):
-                    switch error {
+        Task {
+            do {
+                appetizers = try await NetworkManager.shared.getAppetizers()
+            } catch {
+                if let apError = error as? APError {
+                    switch apError {
                     case .invalidURL:
                         alertItem = AlertContext.invalidURL
                         
@@ -37,8 +66,11 @@ final class AppetizerListViewModel: ObservableObject {
                     case .unableToComplete:
                         alertItem = AlertContext.unableToComplete
                     }
+                } else {
+                    alertItem = AlertContext.invalidResponse
                 }
             }
+            isLoading = false
         }
     }
 }
